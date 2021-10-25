d9e702b8d582d6f0805a77ee61082a5e1dc48373
Маслов Максим Вячеславович <mmaslov@t1-consulting.ru>
2021-10-25 12:43:41 +0300

    init commit

---
+++ src/test/java/ru/maslov/springstudyprpject/SpringStudyPrpjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maslov.springstudyprpject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyPrpjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
