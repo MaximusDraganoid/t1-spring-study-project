f9a0593e269331ba443f7172a6176a785170baaa
Маслов Максим Вячеславович <mmaslov@t1-consulting.ru>
2021-11-03 14:58:14 +0300

    adding path parametrs in request validations

--- src/main/java/ru/maslov/springstudyprpject/controllers/DoctorController.java
+++ src/main/java/ru/maslov/springstudyprpject/controllers/DoctorController.java
@@ -2,6 +2,7 @@ package ru.maslov.springstudyprpject.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ru.maslov.springstudyprpject.entities.Doctor;
@@ -9,8 +10,10 @@ import ru.maslov.springstudyprpject.errors.ApiError;
 import ru.maslov.springstudyprpject.servicies.DoctorService;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping(path = "doctors")
 public class DoctorController {
@@ -27,12 +30,12 @@ public class DoctorController {
     }
 
     @GetMapping(path = "/{id}")
-    public Doctor getDoctorById(@PathVariable("id") Long id) {
+    public Doctor getDoctorById(@PathVariable("id") @NotNull Long id) {
         return doctorService.getById(id);
     }
 
     @DeleteMapping(path = "/{id}")
-    public void deleteDoctorById(@PathVariable("id") Long id) {
+    public void deleteDoctorById(@PathVariable("id") @NotNull Long id) {
         doctorService.deleteById(id);
     }
 
