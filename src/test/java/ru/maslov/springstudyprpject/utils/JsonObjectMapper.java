7f15c0c74e59865b2c51fd9638f506ab2da82b4e
Maksim Maslov <maslowmaxim@gmail.com>
2021-11-29 15:40:30 +0300

    add comments for object mapper

--- src/test/java/ru/maslov/springstudyprpject/utils/JsonObjectMapper.java
+++ src/test/java/ru/maslov/springstudyprpject/utils/JsonObjectMapper.java
@@ -3,6 +3,8 @@ package ru.maslov.springstudyprpject.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonObjectMapper {
+    //ObjectMapper класс который мапит объекты в json
+    
     public static String asJsonString(Object object) {
         try {
             return new ObjectMapper().writeValueAsString(object);
