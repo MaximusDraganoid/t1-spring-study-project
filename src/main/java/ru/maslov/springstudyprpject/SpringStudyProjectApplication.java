92de041b746f574992067896ef92d39c9c1c7949
MaximusDraganoid <maslowmaxim@gmail.com>
2021-12-06 14:11:04 +0300

    add swagger ro project

--- src/main/java/ru/maslov/springstudyprpject/SpringStudyProjectApplication.java
+++ src/main/java/ru/maslov/springstudyprpject/SpringStudyProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.math.BigInteger;
+
 @SpringBootApplication
 public class SpringStudyProjectApplication {
 
@@ -16,5 +18,9 @@ public class SpringStudyProjectApplication {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    void fictiveFunction() {
+        BigInteger big;
     }
 }
